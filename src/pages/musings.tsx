8e620e9c90644ed3132d40a704595d0b9db1fbca
Andrew Leung <andrewleung104@gmail.com>
2023-01-13 16:01:24 -0500

    Organize bit components

--- src/pages/musings.tsx
+++ src/pages/musings.tsx
@@ -7,6 +7,7 @@ import type { StructuredTextGraphQlResponse } from "react-datocms";
 import { renderRule } from "react-datocms";
 import { StructuredText } from "react-datocms";
 import Highlight, { defaultProps } from "prism-react-renderer";
+import { Code } from "datocms-structured-text-utils";
 import { isCode } from "datocms-structured-text-utils";
 import { z } from "zod";
 import vsDark from "prism-react-renderer/themes/vsDark";
@@ -16,6 +17,74 @@ import { useEffect, useState } from "react";
 
 type Language = React.ComponentProps<typeof Highlight>["language"];
 const language: z.ZodType<Language> = z.enum(["typescript", "python"]);
+type Bit = BitsQuery["allBits"][number];
+
+function Code({ node, theme }: { node: Code; theme: string }) {
+  const codeTheme = theme === "dark" ? vsDark : vsLight;
+
+  return (
+    <Highlight
+      {...defaultProps}
+      code={node.code}
+      language={language.parse(node.language || "unknown")}
+      theme={codeTheme}
+    >
+      {({ className, style, tokens, getLineProps, getTokenProps }) => (
+        <pre className={className} style={style}>
+          {tokens.map((line, i) => (
+            <div key={i}>
+              <span className="select-none pr-3 text-gray-400 dark:text-gray-600">
+                {i + 1}
+              </span>
+              <span {...getLineProps({ line, key: i })}>
+                {line.map((token, key) => (
+                  <span key={key} {...getTokenProps({ token, key })} />
+                ))}
+              </span>
+            </div>
+          ))}
+        </pre>
+      )}
+    </Highlight>
+  );
+}
+
+function BitTitle({ title }: { title: Bit["title"] }) {
+  return <div className="text-3xl">{title}</div>;
+}
+
+function BitContent({
+  content,
+  theme,
+}: {
+  content: Bit["content"];
+  theme: string;
+}) {
+  return (
+    <StructuredText
+      // TODO: Figure out how to type this.
+      data={content?.value as StructuredTextGraphQlResponse}
+      customNodeRules={[
+        renderRule(isCode, ({ node }) => {
+          return <Code node={node} theme={theme} />;
+        }),
+      ]}
+    />
+  );
+}
+function BitTags({ tags }: { tags: Bit["tags"] }) {
+  return (
+    <>
+      {tags.map((tag) => (
+        <span key={tag.id}>#{tag.tag}</span>
+      ))}
+    </>
+  );
+}
+
+function Bit({ children }: { children: React.ReactNode }) {
+  return <div>{children}</div>;
+}
 
 export default function Musings({ result }: { result: BitsQuery }) {
   const { theme } = useTheme();
@@ -25,66 +94,24 @@ export default function Musings({ result }: { result: BitsQuery }) {
     setIsMounted(true);
   }, []);
 
-  if (!isMounted) return <div>foo</div>;
-
-  const codeTheme = theme === "dark" ? vsDark : vsLight;
+  // TODO: Render a proper skeleton.
+  if (!isMounted || theme === undefined) return <div>foo</div>;
 
   return (
     <Container>
       {result.allBits.map((bit) => (
-        <div key={bit.id}>
-          <div className="text-3xl">{bit.title}</div>{" "}
-          {bit.tags.map((tag) => (
-            <span key={tag.id}>#{tag.tag}</span>
-          ))}
-          <StructuredText
-            data={bit.content.value as StructuredTextGraphQlResponse}
-            customNodeRules={[
-              renderRule(isCode, ({ node, key }) => {
-                return (
-                  <Highlight
-                    {...defaultProps}
-                    code={node.code}
-                    language={language.parse(node.language || "unknown")}
-                    theme={codeTheme}
-                  >
-                    {({
-                      className,
-                      style,
-                      tokens,
-                      getLineProps,
-                      getTokenProps,
-                    }) => (
-                      <pre className={className} style={style}>
-                        {tokens.map((line, i) => (
-                          <div key={i}>
-                            <span className="select-none pr-3 text-gray-400 dark:text-gray-600">
-                              {i + 1}
-                            </span>
-                            <span {...getLineProps({ line, key: i })}>
-                              {line.map((token, key) => (
-                                <span
-                                  key={key}
-                                  {...getTokenProps({ token, key })}
-                                />
-                              ))}
-                            </span>
-                          </div>
-                        ))}
-                      </pre>
-                    )}
-                  </Highlight>
-                );
-              }),
-            ]}
-          />
-        </div>
+        <Bit key={bit.id}>
+          <BitTitle title={bit.title} />
+          <BitTags tags={bit.tags} />
+          <BitContent content={bit.content} theme={theme} />
+        </Bit>
       ))}
     </Container>
   );
 }
 
 export const getStaticProps: GetStaticProps = async () => {
+  // TODO: Paginate in the future rather than requesting everything?
   const result = await request(BitsDocument);
 
   return {
