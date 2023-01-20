361b1f84f2d160e9d7f1121337a36408a654b182
Andrew Leung <andrewleung104@gmail.com>
2023-01-19 21:39:25 -0500

    Set menu stroke width to 0.75

--- src/components/Container.tsx
+++ src/components/Container.tsx
@@ -25,7 +25,7 @@ function MenuIcon({ className }: { className?: string }) {
     <svg
       xmlns="http://www.w3.org/2000/svg"
       viewBox="0 0 24 24"
-      strokeWidth="1"
+      strokeWidth="0.75"
       stroke="currentColor"
       fill="none"
       strokeLinecap="round"
