7919a0070b993260655f4842f3b5be902418f757
Andrew Leung <andrewleung104@gmail.com>
2023-02-03 11:51:07 -0500

    Simplify spotify component

--- src/components/SpotifyCurrentlyListening.tsx
+++ src/components/SpotifyCurrentlyListening.tsx
@@ -1,36 +1,5 @@
 import type { SpotifyPlayableItem } from "../server/spotify";
-import cn from "classnames";
-import { DeviceSpeakerIcon } from "./Icons";
 import Link from "next/link";
-import Image from "next/image";
-import { useState } from "react";
-
-function Track({
-  track,
-  className,
-}: {
-  track: SpotifyPlayableItem;
-  className?: string;
-}) {
-  const [, type, id] = track.uri.split(":");
-  const href = `https://open.spotify.com/${type as string}/${id as string}`;
-  const image = track.album.images.find(Boolean)?.url;
-
-  return (
-    <Link href={href} className={className}>
-      <div className="relative h-36 w-36">
-        {image && (
-          <Image
-            alt="Currently listening cover art"
-            src={image}
-            className="object-cover"
-            fill
-          />
-        )}
-      </div>
-    </Link>
-  );
-}
 
 export default function SpotifyCurrentlyListening({
   topTracks,
@@ -54,33 +23,15 @@ export default function SpotifyCurrentlyListening({
 
   const [, type, id] = lastPlayedTrack.uri.split(":");
   const href = `https://open.spotify.com/${type as string}/${id as string}`;
-  // const image = lastPlayedTrack.album.images.find(Boolean)?.url;
+
+  const preamble = isCurrentlyPlaying ? "Listening to" : "Last listened to";
 
   return (
-    <div className={cn("flex flex-row items-center", className)}>
-      <div>
-        {isCurrentlyPlaying ? (
-          <span className="flex flex-row items-center">
-            Listening to{" "}
-            <Link
-              href={href}
-              className="hover:underline dark:hover:text-neutral-300"
-            >
-              {lastPlayedTrack.name} by {artistNames}
-            </Link>
-          </span>
-        ) : (
-          <span>
-            Last listened to{" "}
-            <Link
-              href={href}
-              className="hover:underline dark:hover:text-neutral-300"
-            >
-              {lastPlayedTrack.name} by {artistNames}
-            </Link>
-          </span>
-        )}
-      </div>
+    <div className={className}>
+      {preamble}{" "}
+      <Link href={href} className="hover:underline dark:hover:text-neutral-300">
+        {lastPlayedTrack.name} by {artistNames}
+      </Link>
     </div>
   );
 }
