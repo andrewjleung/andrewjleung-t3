43721cfa10b259b41cd9a1af109278b7a7a327dd
Andrew Leung <andrewleung104@gmail.com>
2023-03-16 11:59:14 -0400

    Remove icons and fonts from home page file

--- src/components/Fonts.ts
+++ src/components/Fonts.ts
@@ -1,5 +1,7 @@
-import { Inter } from "@next/font/google";
+import { Inter, Roboto_Mono } from "@next/font/google";
 
 export const inter300 = Inter({ weight: "300", subsets: ["latin"] });
 export const inter700 = Inter({ weight: "700", subsets: ["latin"] });
 export const inter800 = Inter({ weight: "800", subsets: ["latin"] });
+
+export const RobotoMono300 = Roboto_Mono({ weight: "300", subsets: ["latin"] });
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,5 @@
 import Container from "../components/Container";
-import { inter700, inter800 } from "../components/Fonts";
+import { inter700, inter800, RobotoMono300 } from "../components/Fonts";
 import cn from "classnames";
 import Balancer from "react-wrap-balancer";
 import useInterval from "../hooks/useInterval";
@@ -8,17 +8,23 @@ import Link from "next/link";
 import Image from "next/image";
 import Layout from "../components/Layout";
 import useIntersection from "../hooks/useIntersection";
-import { CodeIcon, DeviceSpeakerIcon } from "../components/Icons";
+import {
+  CodeIcon,
+  DeviceSpeakerIcon,
+  GitHubIcon,
+  LinkedInIcon,
+  MapPinIcon,
+  RightChevronIcon,
+  SpotifyIcon,
+  YouTubeIcon,
+} from "../components/Icons";
 import relativeTime from "dayjs/plugin/relativeTime";
 import dayjs from "dayjs";
 import Card from "../components/Card";
 import { api } from "../utils/api";
-import { Roboto_Mono } from "@next/font/google";
 import type { SpotifyPlayableItem } from "../server/spotify";
 import type { getLastCommitFromEvents } from "../server/github";
 import { z } from "zod";
-
-const RobotoMono300 = Roboto_Mono({ weight: "300", subsets: ["latin"] });
 
 dayjs.extend(relativeTime);
 
@@ -113,229 +119,6 @@ const PROJECTS: Project[] = [
   },
 ];
 
-function HandStopIcon({ className }: { className?: string }) {
-  return (
-    <svg
-      xmlns="http://www.w3.org/2000/svg"
-      viewBox="0 0 24 24"
-      strokeWidth="2"
-      stroke="currentColor"
-      fill="none"
-      strokeLinecap="round"
-      strokeLinejoin="round"
-      className={className}
-    >
-      <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
-      <path d="M8 13v-7.5a1.5 1.5 0 0 1 3 0v6.5"></path>
-      <path d="M11 5.5v-2a1.5 1.5 0 1 1 3 0v8.5"></path>
-      <path d="M14 5.5a1.5 1.5 0 0 1 3 0v6.5"></path>
-      <path d="M17 7.5a1.5 1.5 0 0 1 3 0v8.5a6 6 0 0 1 -6 6h-2h.208a6 6 0 0 1 -5.012 -2.7a69.74 69.74 0 0 1 -.196 -.3c-.312 -.479 -1.407 -2.388 -3.286 -5.728a1.5 1.5 0 0 1 .536 -2.022a1.867 1.867 0 0 1 2.28 .28l1.47 1.47"></path>
-    </svg>
-  );
-}
-
-function BooksIcon({ className }: { className?: string }) {
-  return (
-    <svg
-      xmlns="http://www.w3.org/2000/svg"
-      viewBox="0 0 24 24"
-      strokeWidth="2"
-      stroke="currentColor"
-      fill="none"
-      strokeLinecap="round"
-      strokeLinejoin="round"
-      className={className}
-    >
-      <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
-      <path d="M5 4m0 1a1 1 0 0 1 1 -1h2a1 1 0 0 1 1 1v14a1 1 0 0 1 -1 1h-2a1 1 0 0 1 -1 -1z"></path>
-      <path d="M9 4m0 1a1 1 0 0 1 1 -1h2a1 1 0 0 1 1 1v14a1 1 0 0 1 -1 1h-2a1 1 0 0 1 -1 -1z"></path>
-      <path d="M5 8h4"></path>
-      <path d="M9 16h4"></path>
-      <path d="M13.803 4.56l2.184 -.53c.562 -.135 1.133 .19 1.282 .732l3.695 13.418a1.02 1.02 0 0 1 -.634 1.219l-.133 .041l-2.184 .53c-.562 .135 -1.133 -.19 -1.282 -.732l-3.695 -13.418a1.02 1.02 0 0 1 .634 -1.219l.133 -.041z"></path>
-      <path d="M14 9l4 -1"></path>
-      <path d="M16 16l3.923 -.98"></path>
-    </svg>
-  );
-}
-
-function BriefcaseIcon({ className }: { className?: string }) {
-  return (
-    <svg
-      xmlns="http://www.w3.org/2000/svg"
-      viewBox="0 0 24 24"
-      strokeWidth="2"
-      stroke="currentColor"
-      fill="none"
-      strokeLinecap="round"
-      strokeLinejoin="round"
-      className={className}
-    >
-      <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
-      <rect x="3" y="7" width="18" height="13" rx="2"></rect>
-      <path d="M8 7v-2a2 2 0 0 1 2 -2h4a2 2 0 0 1 2 2v2"></path>
-      <line x1="12" y1="12" x2="12" y2="12.01"></line>
-      <path d="M3 13a20 20 0 0 0 18 0"></path>
-    </svg>
-  );
-}
-
-function LightbulbIcon({ className }: { className?: string }) {
-  return (
-    <svg
-      xmlns="http://www.w3.org/2000/svg"
-      viewBox="0 0 24 24"
-      strokeWidth="2"
-      stroke="currentColor"
-      fill="none"
-      strokeLinecap="round"
-      strokeLinejoin="round"
-      className={className}
-    >
-      <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
-      <polyline points="12 3 20 7.5 20 16.5 12 21 4 16.5 4 7.5 12 3"></polyline>
-      <line x1="12" y1="12" x2="20" y2="7.5"></line>
-      <line x1="12" y1="12" x2="12" y2="21"></line>
-      <line x1="12" y1="12" x2="4" y2="7.5"></line>
-    </svg>
-  );
-}
-
-function EmailIcon({ className }: { className?: string }) {
-  return (
-    <svg
-      xmlns="http://www.w3.org/2000/svg"
-      viewBox="0 0 24 24"
-      strokeWidth="2"
-      stroke="currentColor"
-      fill="none"
-      strokeLinecap="round"
-      strokeLinejoin="round"
-      className={className}
-    >
-      <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
-      <rect x="3" y="5" width="18" height="14" rx="2"></rect>
-      <polyline points="3 7 12 13 21 7"></polyline>
-    </svg>
-  );
-}
-
-function MapPinIcon({ className }: { className?: string }) {
-  return (
-    <svg
-      xmlns="http://www.w3.org/2000/svg"
-      viewBox="0 0 24 24"
-      strokeWidth="2"
-      stroke="currentColor"
-      fill="none"
-      strokeLinecap="round"
-      strokeLinejoin="round"
-      className={className}
-    >
-      <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
-      <circle cx="12" cy="11" r="3"></circle>
-      <path d="M17.657 16.657l-4.243 4.243a2 2 0 0 1 -2.827 0l-4.244 -4.243a8 8 0 1 1 11.314 0z"></path>
-    </svg>
-  );
-}
-
-function GitHubIcon({ className }: { className?: string }) {
-  return (
-    <svg
-      xmlns="http://www.w3.org/2000/svg"
-      viewBox="0 0 24 24"
-      strokeWidth="1"
-      stroke="currentColor"
-      fill="none"
-      strokeLinecap="round"
-      strokeLinejoin="round"
-      className={className}
-    >
-      <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
-      <path d="M9 19c-4.3 1.4 -4.3 -2.5 -6 -3m12 5v-3.5c0 -1 .1 -1.4 -.5 -2c2.8 -.3 5.5 -1.4 5.5 -6a4.6 4.6 0 0 0 -1.3 -3.2a4.2 4.2 0 0 0 -.1 -3.2s-1.1 -.3 -3.5 1.3a12.3 12.3 0 0 0 -6.2 0c-2.4 -1.6 -3.5 -1.3 -3.5 -1.3a4.2 4.2 0 0 0 -.1 3.2a4.6 4.6 0 0 0 -1.3 3.2c0 4.6 2.7 5.7 5.5 6c-.6 .6 -.6 1.2 -.5 2v3.5"></path>
-    </svg>
-  );
-}
-
-function LinkedInIcon({ className }: { className?: string }) {
-  return (
-    <svg
-      xmlns="http://www.w3.org/2000/svg"
-      viewBox="0 0 24 24"
-      strokeWidth="1"
-      stroke="currentColor"
-      fill="none"
-      strokeLinecap="round"
-      strokeLinejoin="round"
-      className={className}
-    >
-      <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
-      <rect x="4" y="4" width="16" height="16" rx="2"></rect>
-      <line x1="8" y1="11" x2="8" y2="16"></line>
-      <line x1="8" y1="8" x2="8" y2="8.01"></line>
-      <line x1="12" y1="16" x2="12" y2="11"></line>
-      <path d="M16 16v-3a2 2 0 0 0 -4 0"></path>
-    </svg>
-  );
-}
-
-function SpotifyIcon({ className }: { className?: string }) {
-  return (
-    <svg
-      xmlns="http://www.w3.org/2000/svg"
-      viewBox="0 0 24 24"
-      strokeWidth="1"
-      stroke="currentColor"
-      fill="none"
-      strokeLinecap="round"
-      strokeLinejoin="round"
-      className={className}
-    >
-      <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
-      <circle cx="12" cy="12" r="9"></circle>
-      <path d="M8 11.973c2.5 -1.473 5.5 -.973 7.5 .527"></path>
-      <path d="M9 15c1.5 -1 4 -1 5 .5"></path>
-      <path d="M7 9c2 -1 6 -2 10 .5"></path>
-    </svg>
-  );
-}
-
-function YouTubeIcon({ className }: { className?: string }) {
-  return (
-    <svg
-      xmlns="http://www.w3.org/2000/svg"
-      viewBox="0 0 24 24"
-      strokeWidth="1"
-      stroke="currentColor"
-      fill="none"
-      strokeLinecap="round"
-      strokeLinejoin="round"
-      className={className}
-    >
-      <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
-      <rect x="3" y="5" width="18" height="14" rx="4"></rect>
-      <path d="M10 9l5 3l-5 3z"></path>
-    </svg>
-  );
-}
-
-function RightChevronIcon({ className }: { className?: string }) {
-  return (
-    <svg
-      xmlns="http://www.w3.org/2000/svg"
-      viewBox="0 0 24 24"
-      strokeWidth="2"
-      stroke="currentColor"
-      fill="none"
-      strokeLinecap="round"
-      strokeLinejoin="round"
-      className={className}
-    >
-      <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
-      <path d="M9 6l6 6l-6 6"></path>
-    </svg>
-  );
-}
-
 function SectionNavItem({
   id,
   title,
@@ -385,22 +168,6 @@ function SectionNav({ children }: { children: React.ReactNode }) {
         { "hover:px-8": panelNavCanAnimate }
       )}
     >
-      {children}
-    </div>
-  );
-}
-
-function Section({
-  id,
-  children,
-  className,
-}: {
-  id?: string;
-  children: React.ReactNode;
-  className?: string;
-}) {
-  return (
-    <div id={id} className={cn("bg-transparent", className)}>
       {children}
     </div>
   );
@@ -808,54 +575,52 @@ export default function Home() {
         id="container"
         className="flex grow items-center justify-center"
       >
-        <Section id="top-section" className="">
-          <Layout className="relative flex h-full w-full flex-col items-center justify-center px-6">
-            <div className="relative flex flex-col">
-              <div
-                className={cn(
-                  inter700.className,
-                  "relative whitespace-nowrap text-5xl motion-safe:animate-fade-up-0 sm:text-6xl"
-                )}
-              >
-                Andrew Leung
-              </div>
-              <Balancer
-                ratio={1}
-                as="div"
-                className="mt-6 text-base text-black motion-safe:animate-fade-up-1 dark:text-neutral-300 sm:text-lg md:text-xl"
-              >
-                Software engineer seeking full-time, full-stack opportunities.
-                Looking to improve the lives of developers and users alike.
-              </Balancer>
-              <Stats className="mt-6 text-xs text-black motion-safe:animate-fade-up-2 dark:text-neutral-400 sm:text-sm" />
-              <div className="mt-8 flex flex-row items-center gap-3 text-sm text-black motion-safe:animate-fade-up-2 dark:text-neutral-400">
-                <IconLink
-                  href="https://github.com/andrewjleung"
-                  Icon={GitHubIcon}
-                />
-                <IconLink
-                  href="https://www.linkedin.com/in/andrewjleung-"
-                  Icon={LinkedInIcon}
-                />
-                <IconLink
-                  href="https://open.spotify.com/artist/00zDjeTQDVOFlNttOnv9bc"
-                  Icon={SpotifyIcon}
-                />
-                <IconLink
-                  href="https://www.youtube.com/channel/UCVxaN-2GATE-3Ag9RTGrIXw"
-                  Icon={YouTubeIcon}
-                />
-                <Link
-                  href="https://raw.githubusercontent.com/andrewjleung/resumes/main/AndrewLeung_Resume.pdf"
-                  className="ml-2 flex w-fit flex-row items-center gap-1 rounded-full border-1 border-black px-4 py-2 transition-all duration-200 hover:bg-black hover:text-white dark:border-neutral-400 dark:hover:border-white dark:hover:bg-transparent dark:hover:text-white"
-                >
-                  <span className="whitespace-nowrap text-sm">My resume</span>
-                  <RightChevronIcon className="h-4 w-4" />
-                </Link>
-              </div>
+        <Layout className="relative flex h-full w-full flex-col items-center justify-center px-6">
+          <div className="relative flex flex-col">
+            <div
+              className={cn(
+                inter700.className,
+                "relative whitespace-nowrap text-5xl motion-safe:animate-fade-up-0 sm:text-6xl"
+              )}
+            >
+              Andrew Leung
             </div>
-          </Layout>
-        </Section>
+            <Balancer
+              ratio={1}
+              as="div"
+              className="mt-6 text-base text-black motion-safe:animate-fade-up-1 dark:text-neutral-300 sm:text-lg md:text-xl"
+            >
+              Software engineer seeking full-time, full-stack opportunities.
+              Looking to improve the lives of developers and users alike.
+            </Balancer>
+            <Stats className="mt-6 text-xs text-black motion-safe:animate-fade-up-2 dark:text-neutral-400 sm:text-sm" />
+            <div className="mt-8 flex flex-row items-center gap-3 text-sm text-black motion-safe:animate-fade-up-2 dark:text-neutral-400">
+              <IconLink
+                href="https://github.com/andrewjleung"
+                Icon={GitHubIcon}
+              />
+              <IconLink
+                href="https://www.linkedin.com/in/andrewjleung-"
+                Icon={LinkedInIcon}
+              />
+              <IconLink
+                href="https://open.spotify.com/artist/00zDjeTQDVOFlNttOnv9bc"
+                Icon={SpotifyIcon}
+              />
+              <IconLink
+                href="https://www.youtube.com/channel/UCVxaN-2GATE-3Ag9RTGrIXw"
+                Icon={YouTubeIcon}
+              />
+              <Link
+                href="https://raw.githubusercontent.com/andrewjleung/resumes/main/AndrewLeung_Resume.pdf"
+                className="ml-2 flex w-fit flex-row items-center gap-1 rounded-full border-1 border-black px-4 py-2 transition-all duration-200 hover:bg-black hover:text-white dark:border-neutral-400 dark:hover:border-white dark:hover:bg-transparent dark:hover:text-white"
+              >
+                <span className="whitespace-nowrap text-sm">My resume</span>
+                <RightChevronIcon className="h-4 w-4" />
+              </Link>
+            </div>
+          </div>
+        </Layout>
       </Container>
     </div>
   );
