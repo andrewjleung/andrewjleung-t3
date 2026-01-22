46ca8eb52b610d979e4f35ee504c3b2cff194e1f
Andrew Leung <andrewleung104@gmail.com>
2026-01-22 17:26:12 -0600

    Setup blog; dynamic breadcrumbs

---
+++ astro/src/blog/belief.md
@@ -0,0 +1,7 @@
+---
+title: "Belief"
+description: "Attention, and the slow, steady cascade of self belief I stumbled up through my first post-grad job hop."
+publishDate: "2022-08-26"
+---
+
+## WIP
---
+++ astro/src/content.config.ts
@@ -0,0 +1,28 @@
+// 1. Import utilities from `astro:content`
+import { defineCollection } from "astro:content";
+
+// 2. Import loader(s)
+import { glob } from "astro/loaders";
+
+// 3. Import Zod
+import { z } from "astro/zod";
+
+// 4. Define your collection(s)
+const blog = defineCollection({
+    loader: glob({ pattern: "**/*.md", base: "./src/blog" }),
+    schema: z
+        .object({
+            title: z.string(),
+            description: z.string(),
+            publishDate: z.coerce.date(),
+            updateDate: z.coerce.date().optional(),
+            live: z.boolean().default(false),
+        })
+        .transform((data) => ({
+            ...data,
+            updateDate: data.updateDate || data.publishDate,
+        })),
+});
+
+// 5. Export a single `collections` object to register your collection(s)
+export const collections = { blog };
