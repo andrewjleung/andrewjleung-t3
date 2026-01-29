aabe5216da3cf41b7734ada44f0ab3eabc2ca35a
Andrew Leung <andrewleung104@gmail.com>
2026-01-28 22:01:08 -0600

    Fix imports in astro config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,9 @@
 // @ts-check
-import { defineConfig } from "astro/config";
 
-import tailwindcss from "@tailwindcss/vite";
 import markdoc from "@astrojs/markdoc";
-
 import react from "@astrojs/react";
+import tailwindcss from "@tailwindcss/vite";
+import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
@@ -19,4 +18,3 @@ export default defineConfig({
         "/linkedin": "https://linkedin.com/in/andrewjleung-",
     },
 });
-
